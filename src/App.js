dfec664109cebe4ad529b78a0a6478a82deb7980
jeancar1323 <jeancar1323@gmail.com>
2020-03-04 20:36:43 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+import styles from './styles/styles'
+
+class App extends Component{
+  render(){
+    return(
+    <View style={styles.tempo}>
+      <Text style={styles.tempTexto}>Em construção</Text>
+    </View>)
+  }
+}
+
+
+
+
+export default App;
---
+++ src/styles/styles.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {StyleSheet} from 'react-native'
+
+
+export default temp = StyleSheet.create({
+  tempo:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    },
+    tempTexto:{
+      fontSize:30,
+      fontWeight:'bold',
+    }
+})
